6a377d9c2a52def804ed14ab82b68f566d9ac6b8
kalkal289 <1205ryuki@gmail.com>
2023-10-05 12:25:15 +0900

    マイページ上部のbladeファイル共通化・プロフィール画像の表示調整

--- resources/views/users/mypage_post.blade.php
+++ resources/views/users/mypage_post.blade.php
@@ -9,61 +9,11 @@
     <x-app-layout>
         <body>
             <div class="w-1/2 mx-auto pb-10">
-                <div class="flex justify-between mt-4">
-                    <div class="flex">
-                        <div class="w-16 h-16 border border-black rounded-full">
-                            @if($user->profile_image)
-                                <img src="{{ $user->profile_image }}" alt="プロフィール画像"/>
-                            @else
-                                <img src="https://res.cloudinary.com/drs9gzes2/image/upload/v1695132757/kkrn_icon_user_14_evxlot.png" alt="プロフィール画像"/>
-                            @endif
-                        </div>
-                        <div>
-                            <h1 class="font-bold">{{ $user->name }}</h1>
-                            <p>@ {{ $user->account_name }}</p>
-                            <div>
-                                @foreach ($user->usertags as $usertag)
-                                    <span class="mr-4 text-blue-400">#{{ $usertag->name }}</span>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="border border-black rounded mt-6 mr-6">
-                            @if($user->id == Auth::id())
-                                <a href="/mypages/profile/{{ Auth::id() }}" class="p-2">プロフィール編集</a>
-                            @else
-                                @if($user->is_followed_by_auth_user())
-                                    <a href="/unfollow/{{ $user->id }}" class="p-2">フォロー解除</a>
-                                @else
-                                    <a href="/follow/{{ $user->id }}" class="p-2">フォロー</a>
-                                @endif
-                            @endif
-                        </div>
-                    </div>
-                </div>
-                <div class="p-4 my-4">
-                    <p>{{ $user->message }}</p>
-                </div>
-                <div class="flex justify-between pb-6 border-b-2 border-black">
-                    <div class="flex">
-                        <p>
-                            <a href="/followeds/{{ $user->id }}">フォロー中 {{ $user->followeds()->count() }}</a>
-                        </p>
-                        <p class="ml-4">
-                            <a href="/followers/{{ $user->id }}">フォロワー {{ $user->followers()->count() }}</a>
-                        </p>
-                    </div>
-                    <div class="mr-10">
-                        <a href="/mypages/likes/{{ $user->id }}">いいねした投稿一覧</a>
-                    </div>
-                </div>
-                <div class="flex justify-around border-b-2 border-black mb-4">
-                    <a href="/mypages/{{ $user->id }}" class="w-1/4 py-2 text-center">Main</a>
-                    <a href="/mypages/posts/{{ $user->id }}" class="w-1/4 py-2 text-center {{ ($kind == 0) ? "bg-gray-600 text-white" : "" }}">Post</a>
-                    <a href="/mypages/big/{{ $user->id }}" class="w-1/4 py-2 text-center {{ ($kind == 1) ? "bg-gray-600 text-white" : "" }}">BigPost</a>
-                    <a href="/mypages/store/{{ $user->id }}" class="w-1/4 py-2 text-center">Store</a>
-                </div>
+                
+                @include('users.mypage_common', [
+                    'user' => $user,
+                    'kind' => $kind,
+                ])
                 
                 <div class='contents mt-10'>
                     @if(count($posts) == 0)

1cb1a4eb133313149774a09c961b6f499d54cd6f
kalkal289 <1205ryuki@gmail.com>
2023-10-14 17:28:38 +0900

    縦画面時の検索結果画面の表示の調整

--- resources/views/mains/filtered.blade.php
+++ resources/views/mains/filtered.blade.php
@@ -13,9 +13,13 @@
                     <main>
                         <div class="center-area">
                             <div class="center-title-area">
-                                <h1 class="center-title"><i class="fa-solid fa-star text-red-300"></i> メインコンテンツ{{ ($is_followed_user) ? " フォロー中のみ" : "" }} 　</h1>
-                                <p class="filter-words">{{ ($keyword) ? "「". $keyword. "」の検索結果" : "" }}</p>
-                                <div class="center-title-border"></div>
+                                <h1 class="center-title"><i class="fa-solid fa-star text-red-300"></i> メインコンテンツ 　</h1>
+                                <ul class="filter-options">
+                                    <li>{{ ($is_followed_user) ? "　フォロー中のみ　" : "" }}</li>
+                                    @if($keyword)
+                                        <li>「 <span class="filter-keyword">{{ $keyword }}</span> 」の検索結果</li>
+                                    @endif
+                                </ul>
                             </div>
                             <div class="center-container">
                                 <div class="center-contents-area">
